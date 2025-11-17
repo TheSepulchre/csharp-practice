c1ab9b31cbaa8125dc0609e3b47896c73a7d3b3e
Matthew Walker <43251061+TheSepulchre@users.noreply.github.com>
2025-11-17 11:33:34 +0000

    setup for naming convention practice

---
+++ readability/Program.cs
@@ -0,0 +1,7 @@
+﻿/*  Learning Objectives
+Choose a descriptive name for variables that describe their purpose and intent.
+Use code comments to temporarily instruct the compiler to ignore lines of code.
+Use code comments to describe higher-level requirements or purpose for a passage of code.
+Write code that effectively uses whitespace to convey the relationship of lines of code.
+*/
+
