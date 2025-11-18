00f230727f25cfd70b4b83a84714a9c739a0b9a5
Matthew Walker <43251061+TheSepulchre@users.noreply.github.com>
2025-11-18 22:05:54 +0000

    working WriteLine with ?:

--- booleans/Program.cs
+++ booleans/Program.cs
@@ -3,6 +3,8 @@
 Random coinFlip = new Random();
 int flipResult = coinFlip.Next(0,2);
 
-flipResult == 0 ? 
-    Console.WriteLine("Heads"): 
-    Console.WriteLine("Tails");
+//flipResult == 0 ? 
+//    Console.WriteLine("Heads"): 
+//    Console.WriteLine("Tails");
+
+Console.WriteLine((flipResult == 0) ? "Heads" : "Tails");
