cbe3dd7e6626454e24fdd839025e98c92ee81da4
Matthew Walker <43251061+TheSepulchre@users.noreply.github.com>
2025-11-18 22:12:30 +0000

    creates working boolean permission code

--- booleans/Program.cs
+++ booleans/Program.cs
@@ -1,10 +1,30 @@
 ﻿// Coin flip challenge
 
-Random coinFlip = new Random();
-int flipResult = coinFlip.Next(0,2);
+//Random coinFlip = new Random();
+//int flipResult = coinFlip.Next(0,2);
 
 //flipResult == 0 ? 
 //    Console.WriteLine("Heads"): 
 //    Console.WriteLine("Tails");
 
-Console.WriteLine((flipResult == 0) ? "Heads" : "Tails");
+//Console.WriteLine((flipResult == 0) ? "Heads" : "Tails");
+
+string permission = "Admin|Manager";
+int level = 56;
+
+if (permission.Contains("Admin"))
+{
+    Console.WriteLine((level > 55) ? 
+        "Welcome, Super Admin user" : 
+        "Welcome, Admin user");
+}
+else if (permission.Contains("Manager"))
+{
+    Console.WriteLine((level >=20) ? 
+        "Contact an Admin for access" : 
+        "You do not have sufficient privileges");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges");
+}
