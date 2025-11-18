b64170e065735ab4cd3467853d112eb8ea7c22bc
Matthew Walker <43251061+TheSepulchre@users.noreply.github.com>
2025-11-18 22:03:25 +0000

    first attempt, conditional operator
    failed
    will look at result to see where it went wrong

--- booleans/Program.cs
+++ booleans/Program.cs
@@ -1,1 +1,8 @@
 ﻿// Coin flip challenge
+
+Random coinFlip = new Random();
+int flipResult = coinFlip.Next(0,2);
+
+flipResult == 0 ? 
+    Console.WriteLine("Heads"): 
+    Console.WriteLine("Tails");
