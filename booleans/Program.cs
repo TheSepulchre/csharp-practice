a1e62c635d3ccef880c482b63d33d2aef11ff994
Matthew Walker <43251061+TheSepulchre@users.noreply.github.com>
2025-11-18 21:50:19 +0000

    learns possible operators

--- booleans/Program.cs
+++ booleans/Program.cs
@@ -1,6 +1,28 @@
-﻿Console.WriteLine("a" == "a");
-Console.WriteLine("a" == "A");
-Console.WriteLine(1 == 2);
+﻿//string value1 = " a";
+//string value2 = "A ";
+//Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
 
-string myValue = "a";
-Console.WriteLine(myValue == "a");
+//Console.WriteLine("a" != "a");
+//Console.WriteLine("a" != "A");
+//Console.WriteLine(1 != 2);
+
+//string myValue = "a";
+//Console.WriteLine(myValue != "a");
+
+//Console.WriteLine(1 > 2);
+//Console.WriteLine(1 < 2);
+//Console.WriteLine(1 >= 1);
+//Console.WriteLine(1 <= 1);
+
+//string pangram = "The quick brown fox jumps over the lazy dog.";
+//Console.WriteLine(pangram.Contains("fox"));
+//Console.WriteLine(pangram.Contains("cow"));
+
+// These two lines of code will create the same output
+
+//Console.WriteLine(pangram.Contains("fox") == false);
+//Console.WriteLine(!pangram.Contains("fox"));
+
+//string pangram = "The quick brown fox jumps over the lazy dog.";
+//Console.WriteLine(!pangram.Contains("fox"));
+//Console.WriteLine(!pangram.Contains("cow"));
